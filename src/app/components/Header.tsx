38bf0417cfdfdfc0e5d9b6c871944d2405a0e314
KaiqueFj <kaiquelferraz@hotmail.com>
2024-11-12 23:13:03 -0300

    feat: added the feature to show in which page you are currently in, making the nav bar get a special color

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation"; // Use usePathname instead of useRouter
 
 export default function Header() {
+  const pathname = usePathname(); // Get the current pathname
+
   return (
     <header className="flex items-center justify-center w-full mt-8">
       <nav className="flex items-center justify-center w-full max-w-screen-md bg-secondary-background rounded-full ring-1 ring-white/10 px-4 py-2">
@@ -8,7 +13,11 @@ export default function Header() {
           <li>
             <Link
               href="/about"
-              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
+              className={`px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition ${
+                pathname === "/about"
+                  ? "text-hover-primary-text-color"
+                  : "text-primary-text-color"
+              }`}
             >
               About
             </Link>
@@ -16,7 +25,11 @@ export default function Header() {
           <li>
             <Link
               href="/projects"
-              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
+              className={`px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition ${
+                pathname === "/projects"
+                  ? "text-hover-primary-text-color"
+                  : "text-primary-text-color"
+              }`}
             >
               Projects
             </Link>
@@ -24,7 +37,11 @@ export default function Header() {
           <li>
             <Link
               href="/hireMe"
-              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
+              className={`px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition ${
+                pathname === "/hireMe"
+                  ? "text-hover-primary-text-color"
+                  : "text-primary-text-color"
+              }`}
             >
               Hire me
             </Link>
