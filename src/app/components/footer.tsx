e9d50c4173718d325f1d8333f54197d87f284390
KaiqueFj <kaiquelferraz@hotmail.com>
2024-11-13 23:20:43 -0300

    refactor: changed a href for Link on footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="mt-8 w-full border-t border-border-color">
@@ -5,24 +7,24 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left gap-4">
           {/* Links Section */}
           <div className="flex gap-6 text-primary-text-color text-sm">
-            <a
-              className="transition hover:text-hover-primary-text-color"
+            <Link
               href="/"
+              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
             >
               About
-            </a>
-            <a
-              className="transition hover:text-hover-primary-text-color"
+            </Link>
+            <Link
               href="/projects"
+              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
             >
               Projects
-            </a>
-            <a
-              className="transition hover:text-hover-primary-text-color"
+            </Link>
+            <Link
               href="/hireMe"
+              className="px-4 py-2 text-primary-text-color font-medium hover:text-hover-primary-text-color transition"
             >
-              Hire Me
-            </a>
+              Hire me
+            </Link>
           </div>
           {/* Copyright Section */}
           <p className="text-primary-text-color text-sm mt-4 md:mt-0">
